f959d0000b8eaad501483c71ed9e84fb4888d411
Bonu <cb185249@ncr.com>
2020-03-14 21:12:02 +0530

    Test TDD

---
+++ StackTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDTest
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Creation()
+        {
+            Stack<int> s = new Stack<int>(3);
+            Assert.AreEqual(0, s.Size);
+
+        }
+    }
+}
